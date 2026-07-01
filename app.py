e75edb2c7d88321b0b749993761f1e209bc24d07
Reshma232002 <reshmathakshinamoorthy@outlook.com>
2026-07-01 21:31:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ st.set_page_config(
     page_icon="🚀",
     layout="wide"
 )
-with open("assets/style.css") as f:
+with open("assets/styles.css") as f:
     st.markdown(
         f"<style>{f.read()}</style>",
         unsafe_allow_html=True
